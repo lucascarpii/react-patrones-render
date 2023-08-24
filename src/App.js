b3965eb3aef00e8dc3f5096c07ce02937a5f5dc5
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-08-24 11:35:50 -0300

    Clase nro 5 - Estilos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
 import React from 'react';
 
 const defaultTodos = [
@@ -15,7 +14,7 @@ const defaultTodos = [
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <TodoCounter completed={3} total={5} />
       <TodoSearch />
 
@@ -30,7 +29,7 @@ function App() {
       </TodoList>
       
     <CreateTodoButton />
-    </React.Fragment>
+    </>
   );
 }
 
