f13f25e878db825e644a10f9f012d4938146b4d1
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-08-25 16:34:14 -0300

    buscando todos

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ function App() {
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    }
+  )
   console.log('Los usuarios buscan todos de ' + searchValue);
 
   return (
@@ -34,7 +42,7 @@ function App() {
       <TodoCounter completed={completedTodos} total={totalTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
