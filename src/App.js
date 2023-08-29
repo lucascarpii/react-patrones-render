ea4e423f4ba14ee199173244b3ae0819e762b8b6
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-08-29 18:40:33 -0300

    Clase 13 a 16, localStorage, customHooks, organizando carpetas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoInput } from './TodoInput';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoInput } from './components/TodoInput';
 import React from 'react';
 
-const defaultTodos = [
-  {text: 'Primer tarea', completed: true},
-  {text: 'Segunda tarea', completed: false},
-  {text: 'Tercera tarea', completed: false},
-  {text: 'Cuarta tarea', completed: false},
-]
+// const defaultTodos = [
+//   {text: 'Primer tarea', completed: true},
+//   {text: 'Segunda tarea', completed: false},
+//   {text: 'Tercera tarea', completed: false},
+//   {text: 'Cuarta tarea', completed: false},
+// ]
+// let stringifiedTodos = JSON.stringify(defaultTodos)
+// localStorage.setItem('TODOS_V1', stringifiedTodos);
+
+// localStorage.removeItem('TODOS_V1');
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+
+  let parsedItem;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -31,24 +57,24 @@ function App() {
   const completeTodo =  (text) => {
     const newTodos = [...todos]
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
       )
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   const deleteTodo =  (text) => {
     const newTodos = [...todos]
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
       )
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   return (
-    <main className='grid grid-cols-2 gap-8 max-w-screen-lg mx-auto mt-12'>
-      <div  className='bg-white p-6 rounded-lg'>
+    <main className='grid grid-cols-2 gap-8 max-w-screen-lg mx-auto mt-12 '>
+      <div  className='bg-white p-6 rounded-lg h-fit'>
       <h2 className='text-center text-lg mb-6 text-indigo-500 font-medium'>
         Crear una nueva tarea
       </h2>
