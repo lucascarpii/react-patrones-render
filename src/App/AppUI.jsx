351f97313afbe7a9ff1ad9815d123ef8d883316f
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-09-02 19:53:23 -0300

    Clase 22, React context... no entiendo nada

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -7,55 +7,61 @@ import { TodoInput } from '../components/TodoInput';
 import { TodosLoading } from '../components/TodosLoading';
 import { TodosError } from '../components/TodosError';
 import { EmptyTodos } from '../components/EmptyTodos';
+import { TodoContext } from '../TodoContext';
 
-function AppUI ({
-  loading, 
-  error,
-  completedTodos,
-  totalTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}){
+function AppUI (){
   return(
   <main className='grid grid-cols-2 gap-8 max-w-screen-lg mx-auto mt-12 '>
     <div  className='bg-white p-6 rounded-lg h-fit'>
-    <h2 className='text-center text-lg mb-6 text-indigo-500 font-medium'>
-      Crear una nueva tarea
-    </h2>
-    <TodoInput />
-    <CreateTodoButton />
+      <h2 className='text-center text-lg mb-6 text-indigo-500 font-medium'>
+        Crear una nueva tarea
+      </h2>
+      <TodoInput />
+      <CreateTodoButton />
     </div>
+    
     <div className='p-6'>
-      <TodoCounter completed={completedTodos} total={totalTodos} loading={loading} />
+      <TodoCounter />
+      <TodoSearch />
       
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} loading={loading} />
-      <TodoList>
-        {loading && (
-          <>
-            <TodosLoading />
-            <TodosLoading />
-            <TodosLoading />
-            <TodosLoading />
-          </>
+      <TodoContext.Consumer>
+        {({
+          loading,
+          error,
+          searchedTodos,
+          completeTodo,
+          deleteTodo,
+        }) => (
+          <TodoList>
+            {loading && (
+              <>
+                <TodosLoading />
+                <TodosLoading />
+                <TodosLoading />
+                <TodosLoading />
+              </>
+            )}
+            {error && <TodosError/>}
+            {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
+
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
         )}
-        {error && <TodosError />}
-        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
-        {searchedTodos.map(todo => (
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed}
-          onComplete={() => completeTodo(todo.text)}
-          onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+      </TodoContext.Consumer>
+
     </div>
   </main>
   )
 }
+
+
 
 export {AppUI}
--- src/TodoContext/useLocalStorage.jsx
+++ src/TodoContext/useLocalStorage.jsx
@@ -1,30 +1,32 @@
 import React from "react";
+
 function useLocalStorage(itemName, initialValue) {
   const [item, setItem] = React.useState(initialValue);
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(false);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     setTimeout(() => {
       try {
         const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
     
+        let parsedItem;
+  
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItem = initialValue;
         } else {
           parsedItem = JSON.parse(localStorageItem);
-          setItem(parsedItem)
+          setItem(parsedItem);
         }
-    
-        setLoading(false)
-      } catch (error) {
-        setLoading(false)
-        setError(true)
+  
+        setLoading(false);
+      } catch(error) {
+        setLoading(false);
+        setError(true);
       }
-    }, 2000 )
-  }, [])
+    }, 2000);
+  });
   
   const saveItem = (newItem) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
